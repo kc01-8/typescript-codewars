448e1861a1a83d799a61b9088fa62503dc94c472
kc01-8 <minecraftandcallofduty101@gmail.com>
2024-01-28 21:44:22 +0200

    Create arrayDiff.js

---
+++ arrayDiff.js
@@ -0,0 +1,12 @@
+function arrayDiff(a, b) {
+ for (let i = 0; i < b.length; i++) {
+   for (let j = 0; j < a.length; j++) {
+    if (b[i] === a[j]) {
+      a.splice(j, 1);
+      j--;
+    }
+   }
+  }
+  return a;
+}
+
